c0e238d7a5a32bc644037fa5fb0742507084a4f6
Jakob <jakob@develly.ee>
2018-08-01 13:13:21 +0300

    ok

---
+++ HelloXamarinFormsWorld.Android/obj/Release/android/src/md59b7d563fcdb4760ed2f1463daf6ba010/MainActivity.java
@@ -0,0 +1,47 @@
+package md59b7d563fcdb4760ed2f1463daf6ba010;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsApplicationActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("HelloXamarinFormsWorld.Droid.MainActivity, HelloXamarinFormsWorld.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("HelloXamarinFormsWorld.Droid.MainActivity, HelloXamarinFormsWorld.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
