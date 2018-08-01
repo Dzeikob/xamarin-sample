e7f8c1352680ab72cac376ab7ea4200139d28386
Jakob <jakob@develly.ee>
2018-08-01 11:38:49 +0300

    ok

--- HelloXamarinFormsWorld.Android/MainActivity.cs
+++ HelloXamarinFormsWorld.Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.OS;
-
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
@@ -12,7 +14,8 @@ namespace HelloXamarinFormsWorld.Droid
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            Microsoft.AppCenter.AppCenter.Start("0b093df6-9196-468a-a7a4-f55044f955f1",
+                   typeof(Analytics), typeof(Crashes));
             Forms.Init(this, bundle);
 
 			LoadApplication (new App ());
